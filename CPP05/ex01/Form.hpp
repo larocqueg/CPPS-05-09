05a1330b289550c18605dac3ebb42ef4dbe79978
larocqueg <portugalgabriel2021@gmail.com>
2025-10-05 14:53:46 +0100

    added pragma once to all .hpp so the compiler does not complain about multi declaration of the same .hp

--- CPP05/ex01/Form.hpp
+++ CPP05/ex01/Form.hpp
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include "Bureaucrat.hpp"
 #include <iostream>
 #include <string>
