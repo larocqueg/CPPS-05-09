0ae235113e5a0ae41398b353c662600f84bc5ae6
larocqueg <portugalgabriel2021@gmail.com>
2025-10-08 15:58:54 +0100

    .hpp

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 11:29:19 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/10/08 11:29:52 by gde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+  private:
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter& original);
+    ScalarConverter& operator=(const ScalarConverter& original);
+    ~ScalarConverter();
+    static const std::string getType(const std::string& literal);
+  public:
+    static void convert(std::string& literal);
+}
