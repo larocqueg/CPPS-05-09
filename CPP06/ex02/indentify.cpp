040b4f5d248608a61a554766998bfdb1fc5fa3f0
larocqueg <portugalgabriel2021@gmail.com>
2025-10-14 18:28:47 +0100

    need to finish main

--- CPP06/ex02/indentify.cpp
+++ CPP06/ex02/indentify.cpp
@@ -31,13 +31,60 @@ Base  *generate(void)
     std::cout << GREEN << "Generating object C!" << RESET << std::endl;
     return (new C);
   }
+  std::cout << RED << "Couldn't generate a instance of A, B or C!" << RESET << std::endl;
   return (NULL);
 }
 
 void  indentify(Base* p)
 {
+  if (dynamic_cast<A*>(p))
+  {
+    std::cout << GREEN << "Object type is A!" << RESET << std::endl;
+  }
+  else if (dynamic_cast<B*>(p))
+  {
+    std::cout << GREEN << "Object type is B!" << RESET << std::endl;
+  }
+  else if (dynamic_cast<C*>(p))
+  {
+    std::cout << GREEN << "Object type is C!" << RESET << std::endl;
+  }
+  else
+  {
+    std::cout << RED << "Invalid object!" << RESET << std::endl;
+  }
 }
 
 void  indentify(Base& p)
 {
+  try
+  {
+    (void)dynamic_cast<A&>(p);
+    std::cout << GREEN << "Object type is A!" << RESET << std::endl;
+    return ;
+  }
+  catch (...)
+  {
+  }
+  
+  try
+  {
+    (void)dynamic_cast<B&>(p);
+    std::cout << GREEN << "Object type is B!" << RESET << std::endl;
+    return ;
+  }
+  catch (...)
+  {
+  }
+  
+  try
+  {
+    (void)dynamic_cast<C&>(p);
+    std::cout << GREEN << "Object type is C!" << RESET << std::endl;
+    return ;
+  }
+  catch (...)
+  {
+  }
+  std::cout << RED << "Invalid object!" << RESET << std::endl;
 }
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   indentify.hpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 18:04:12 by gde-la-r          #+#    #+#             */
-/*   Updated: 2025/10/14 18:05:40 by gde-la-r         ###   ########.fr       */
+/*   Created: 2025/10/14 18:26:43 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/10/14 18:26:54 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
-#include <ctime>
-#include <string>
-#include <cstdlib>
-
-#include "Base.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include "Base.hpp"
+#include "identify.hpp"
 
-Base  *generate(void);
-void  indentify(Base* p);
-void  indentify(Base& p);
+int main(void)
+{
+}
