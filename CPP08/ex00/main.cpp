3e7bed2da7bb683f4e6551e16fdb9dfe6c215686
larocqueg <portugalgabriel2021@gmail.com>
2025-10-22 14:36:08 +0100

    Need to improove main

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -17,13 +17,15 @@ int main(void)
   int arr[] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
   std::vector<int>vectorArr(arr, arr + sizeof(arr)/sizeof(arr[0]));
 
-  std::cout << YELLOW << "\nLooping trought the vector untill it catches a invalid number\n" << RESET << std::endl;
+  std::cout << YELLOW << "\nLooping trought the vector untill it catches a invalid number\n"
+    << RESET << std::endl;
   try
   {
     for  (int i = -5; i <= 6; i++)
     {
       std::vector<int>::iterator it = easyfind(vectorArr, i);
-      std::cout << GREEN << "Found the passed value at position: "<< std::distance(vectorArr.begin(), it) << RESET << std::endl;
+      std::cout << GREEN << "Found the passed value at position: "
+        << std::distance(vectorArr.begin(), it) << RESET << std::endl;
     }
 
   }
@@ -32,11 +34,13 @@ int main(void)
     std::cerr << RED << e.what() << RESET << std::endl;
   }
   
-  std::cout << YELLOW << "\n\nGiving a invalid argument to the easyfind" << RESET << std::endl;
+  std::cout << YELLOW << "\n\nGiving a invalid argument to the easyfind"
+    << RESET << std::endl;
   try
   {
     std::vector<int>::iterator it = easyfind(vectorArr, -10);
-    std::cout << GREEN << "Found the passed value at position: "<< std::distance(vectorArr.begin(), it) << RESET << std::endl;
+    std::cout << GREEN << "Found the passed value at position: "
+      << std::distance(vectorArr.begin(), it) << RESET << std::endl;
   }
   catch (const std::exception& e)
   {
