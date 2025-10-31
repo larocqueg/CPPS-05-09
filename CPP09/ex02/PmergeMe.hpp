72e40e01514968c937f7581483078df7bfcd2bef
larocqueg <portugalgabriel2021@gmail.com>
2025-10-31 14:34:12 +0000

    started ex02

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -1,34 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 19:27:31 by gde-la-r          #+#    #+#             */
-/*   Updated: 2025/10/29 19:31:58 by gde-la-r         ###   ########.fr       */
+/*   Created: 2025/10/31 14:27:23 by gde-la-r          #+#    #+#             */
+/*   Updated: 2025/10/31 14:29:00 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "RPN.hpp"
+#pragma once
 
-int main(int ac, char **av)
+#include <iostream>
+#include <string>
+
+#define RED     "\033[31m"
+#define YELLOW  "\033[33m"
+#define GREEN   "\033[32m"
+#define CYAN    "\033[36m"
+#define RESET   "\033[0m"
+
+class PmergeMe:
 {
-  if (ac != 2)
-  {
-    std::cerr << RED << "Error: Usage: ./RPN \"numbers && signs\"" << RESET << std::endl;
-    return (1);
-  }
-  
-  try 
-  {
-    RPN rpn;
-    rpn.parser(av[1]);
-    rpn.doOp(av[1]);
-  }
-  catch (const std::exception& e)
-  {
-    std::cerr << RED << "Error: " << e.what() << RESET << std::endl;
-  }
-  return (0);
+  private:
+
+  public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& original);
+    PmergeMe& operator=(const PmergeMe& original);
+    ~PmergeMe();
 }
